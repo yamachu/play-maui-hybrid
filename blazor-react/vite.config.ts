db618e663d3ce10b635a9e62f3c420e755488f60
yamachu <yamachu.dev@gmail.com>
2023-12-19 14:46:01 +0900

    Localで動かせなかった…

--- blazor-react/vite.config.ts
+++ blazor-react/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: process.env.TARGET !== 'web' && {
     outDir: '../maui/wwwroot/js',
@@ -15,6 +15,6 @@ export default defineConfig({
   },
   // React自体がprocess.env.NODE_ENVを参照しているので、defineで消し去る
   define: {
-    'process.env.NODE_ENV': '"production"'
-  }
-});
+    'process.env.NODE_ENV': JSON.stringify(mode),
+  },
+}));
