1554ec6a4d03453ecc09712489c44556773d7abd
yamachu <yamachu.dev@gmail.com>
2023-12-19 14:30:29 +0900

    ReactComponent生成サンプルから引っ張ってくる

---
+++ blazor-react/src/hooks/useBlazor.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="@types/react" />
+export function useBlazor(
+  identifier: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  props: any
+): React.ReactElement | null;
