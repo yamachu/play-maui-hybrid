a664e182150f7361ca197ffbed0b2972bc5e3069
yamachu <yamachu.dev@gmail.com>
2023-12-19 14:28:45 +0900

    viteでビルドするためのconfigを追加

--- blazor-react/vite.config.ts
+++ blazor-react/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: process.env.TARGET !== 'web' && {
+    outDir: '../maui/wwwroot/js',
+    lib: {
+      entry: 'src/main.tsx',
+      fileName: 'index',
+      name: 'BlazorReact',
+      formats: ['es'],
+    },
+  },
+  // React自体がprocess.env.NODE_ENVを参照しているので、defineで消し去る
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  }
+});
