d4a563a6d43b09e4751d8969aefcfda9310e0611
yamachu <yamachu.dev@gmail.com>
2023-12-19 16:15:39 +0900

    Blazor側からrenderを行えるように名前付きでexportする

--- blazor-react/src/main.tsx
+++ blazor-react/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const render = () => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+// MAUI非依存の状態でデザインなどを行う用途
+if (import.meta.env.TARGET === "web") {
+  render();
+}
+
+// Blazor MAUI から呼び出せるように
+export const renderReactComponent = render;
