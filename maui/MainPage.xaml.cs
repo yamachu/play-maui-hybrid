e804d60d5c5ea932234e9c59c9a69ebc94121583
yamachu <yamachu.dev@gmail.com>
2023-12-19 16:19:11 +0900

    ReactComponentをmountする

--- maui/MainPage.xaml.cs
+++ maui/MainPage.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace play_maui_hybrid;
+﻿using Microsoft.AspNetCore.Components.Web;
+using play_maui_hybrid.Components.Pages;
+
+namespace play_maui_hybrid;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
+
+		// Register the Blazor WebView component
+		blazorWebView.RootComponents.RegisterForJavaScript<Counter>("BlazorCounter-react");
 	}
 }
